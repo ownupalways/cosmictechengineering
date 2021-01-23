4c50d06c37de3f39323bcac73f82f8d5c7516118
ownupalways <oluwadipegodwin@gmail.com>
2021-01-23 07:28:49 -0800

    menu file update
    menu file update

--- tech.js
+++ tech.js
@@ -1,18 +1,3 @@
-const http = require('http')
-const port = 3000;
-
-const server = http.createServer(function(req, res){
-
-})
-
-server.listen(port,function(error){
-	if(error) {
-		console.log('something went wrong',error)
-	}else{
-		console.log('server is listening on port' + port)
-	}
-})
-
 function adjust() {
 	var x = document.getElementById("power_nav");
 	if (x.className === "topnav") {
